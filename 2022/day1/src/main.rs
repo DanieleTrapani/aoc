08db1005309ccec0cdebbd8a8f2869cb5fac9ccb
Daniele Trapani <daniele.trapani96@gmail.com>
2023-11-25 16:07:17 +0100

    part 2 with error

--- 2022/day1/src/main.rs
+++ 2022/day1/src/main.rs
@@ -1,5 +1,10 @@
 use std::fs;
 
+#[derive(PartialEq, Eq, PartialOrd, Ord)]
+struct Elf {
+    calories: i32,
+}
+
 fn main() {
     let filepath = "data.txt";
     let contents = fs::read_to_string(filepath).unwrap();
@@ -8,31 +13,41 @@ fn main() {
     // increase the counter
 
     // index of the result
-    let mut result = 0;
+    // let mut result = 0;
     // calories count of the max
-    let mut max = 0;
+    // let mut max = 0;
     // index count
-    let mut counter = 0;
+    // let mut counter = 0;
     let mut sum = 0;
+    // vector of elfs
+    let mut elves: Vec<Elf> = Vec::new();
+
     for i in 0..lines.len() {
         if lines[i] == "" {
-            if sum > max {
-                max = sum;
-                result = counter;
-                sum = 0;
-                counter += 1;
-                continue;
-            } else {
-                sum = 0;
-                counter += 1;
-                continue;
-            }
+            let new_elf = Elf { calories: sum };
+            elves.push(new_elf);
+            sum = 0;
+            // if sum > max {
+            //     max = sum;
+            //     result = counter;
+            //     sum = 0;
+            //     counter += 1;
+            //     continue;
+            // } else {
+            //     sum = 0;
+            //     counter += 1;
+            //     continue;
+            // }
         }
         let line_as_int: i32 = lines[i].parse().unwrap();
         sum += line_as_int;
     }
-    println!(
-        "Result: Elf n.{}, they are carrying {} calories",
-        result, max
-    );
+    elves.sort_by(|a, b| b.calories.cmp(&a.calories));
+    let mut result = 0;
+
+    for i in 0..3 {
+        result += elves[i].calories;
+    }
+
+    println!("Result: The top 3 elves are carrying {} calories", result);
 }
